b782289d4e8763288f575a468bfc8a4f33ebe12d
Ivantea <ivantea2004@gmail.com>
2022-06-26 16:33:16 +0300

    const audio buffer

--- include/alya/audio/core/buffer.hpp
+++ include/alya/audio/core/buffer.hpp
@@ -8,4 +8,7 @@ namespace alya::audio::core
 	template<typename WaveFormat>
 	using buffer = std::span<frame<WaveFormat>>;
 
+	template<typename WaveFormat>
+	using const_buffer = std::span<const frame<WaveFormat>>;
+
 }
