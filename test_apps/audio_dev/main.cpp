2843c175c5f12c3d73d28ec13d01bd7cd7d44c64
Ivantea <ivantea2004@gmail.com>
2022-06-26 19:58:44 +0300

    new example

--- test_apps/audio_dev/main.cpp
+++ test_apps/audio_dev/main.cpp
@@ -1,64 +1,69 @@
+
 #include<iostream>
-#include<alya/audio/engine.hpp>
+#include<alya/audio/core/wave_format.hpp>
+#include<alya/audio/core/basic_system_endpoint.hpp>
+#include<alya/audio/core/device_enumerator.hpp>
 #include<alya/audio/basic_sample.hpp>
 #include<alya/resource/file_loader.hpp>
 #include<alya/resource/async_import.hpp>
-#include<boost/asio/thread_pool.hpp>
+#include<boost/asio/io_context.hpp>
+#include<alya/async/delay.hpp>
+#include<alya/audio/basic_context.hpp>
+#include<alya/audio/basic_sample_player.hpp>
 
 using namespace alya;
 
-void show_exception(std::exception_ptr e)
-{
-	try
-	{
-		std::rethrow_exception(e);
-	}
-	catch (const std::exception& e)
-	{
-		std::cout << e.what() << "\n";
-	}
-}
+using wf = audio::core::wave_format<float, 2>;
 
-void run()
+boost::asio::io_context ioc;
+
+async::promise<void> entry()
 {
-	using namespace alya;
 	using namespace std::chrono_literals;
-
 	resource::file_loader loader;
-	boost::asio::thread_pool tp;
-	audio::engine engine;
 
-	std::atomic<bool> end = false;
+	std::cout << "Enter path: ";
+
+	std::string path;
+	std::getline(std::cin, path);
 	
-	([&]()->async::promise<void> {
 
-		auto sample = co_await resource::async_import<audio::basic_sample<audio::engine::wave_format>>(loader, __FILE__"/../1.ogg", tp.get_executor());
+	auto sample = co_await resource::async_import<audio::basic_sample<wf>>(loader, path, ioc.get_executor());
+	
+	audio::core::basic_system_endpoint<wf> ep(audio::core::device_enumerator{}.get_default_device().value(), sample.sample_rate());
 
-		co_await audio::async_play(engine, sample);
-		
-	}
-	)().except(show_exception).then([&]() { end = true; });
+	audio::basic_context<wf> ctx(ep);
 
-	while (!end)
-		std::this_thread::yield();
+	audio::basic_sample_player<wf> sp(sample);
+
+	ctx.set_otput_channel(sp);
+	sp.play();
+	while (sp.is_playing());
+
 }
-//#include<Windows.h>
 
 int main()
 {
-	//SetConsoleOutputCP(CP_UTF8);
+	using namespace std::chrono_literals;
+	using namespace alya;
 
-	try {
+	std::atomic<bool> end = false;
 
-		
-		run();
+	entry().then([&]() { end = true; }).except([&](auto e) {
+		try
+		{
+			std::rethrow_exception(e);
+			}
+		catch (const std::exception& e)
+		{
+			std::cout << e.what();
+		}
+		end = true;
+		});
 
-	}
-	catch (const std::exception&e)
-	{
-		std::cout << e.what();
-		throw;
-	}
+	while (!end)
+		ioc.run();
+
 
 	return 0;
 }
